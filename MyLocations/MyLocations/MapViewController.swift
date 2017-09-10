00b67ae9c9e6594ec67707bbec58799a3d709ad4
glebwell <gleb.maltcev@yandex.ru>
2017-09-10 22:13:42 +0300

    implemented until p.210

--- MyLocations/MyLocations/MapViewController.swift
+++ MyLocations/MyLocations/MapViewController.swift
@@ -12,8 +12,56 @@ import CoreData
 
 class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
+    
+    var managedObjectContext: NSManagedObjectContext! {
+        didSet {
+            NotificationCenter.default.addObserver(forName: Notification.Name.NSManagedObjectContextObjectsDidChange,
+                object: managedObjectContext,
+                queue: OperationQueue.main) { [weak self] notification in
+                    if self != nil, self!.isViewLoaded {
+                        //self!.updateLocations()
 
-    var managedObjectContext: NSManagedObjectContext!
+                        // TODO: implement efficient update of location pins
+                        guard let userInfo = notification.userInfo else { return }
+
+                        if let deletedLocations = userInfo[NSDeletedObjectsKey] as? Set<Location>, !deletedLocations.isEmpty {
+                            print("----- DELETED LOCATIONS -----")
+                            for location in deletedLocations {
+                                print(location)
+                                if let index = self!.locations.index(of: location) {
+                                    self!.locations.remove(at: index)
+                                }
+                                self!.mapView.removeAnnotation(location)
+
+                            }
+                            print("-----------------------------")
+
+                        }
+                        if let insertedLocations = userInfo[NSInsertedObjectsKey] as? Set<Location>, !insertedLocations.isEmpty {
+                            print("----- INSERTED LOCATIONS -----")
+                            for location in insertedLocations {
+                                print(location)
+                                self!.locations.append(location)
+                                self!.mapView.addAnnotation(location)
+                            }
+                            print("------------------------------")
+                        }
+                        if let updatedLocations = userInfo[NSUpdatedObjectsKey] as? Set<Location>, !updatedLocations.isEmpty {
+                            print("----- UPDATED LOCATIONS -----")
+                            for location in updatedLocations {
+                                print(location)
+                                self!.mapView.removeAnnotation(location)
+                                self!.mapView.addAnnotation(location)
+                            }
+                            print("------------------------------")
+                        }
+                        //print(dictionary["inserted"])
+                        //print(dictionary["deleted"])
+                        //print(dictionary["updated"])
+                    }
+                }
+        }
+    }
 
     @IBAction func showUser() {
         let region = MKCoordinateRegionMakeWithDistance(mapView.userLocation.coordinate, 1000, 1000)
@@ -25,7 +73,7 @@ class MapViewController: UIViewController {
         mapView.setRegion(theRegion, animated: true)
     }
 
-    private var locations = [Location]()
+    fileprivate var locations = [Location]()
     private func updateLocations() {
         mapView.removeAnnotations(locations)
 
@@ -77,8 +125,100 @@ class MapViewController: UIViewController {
             showLocations()
         }
     }
+
+    @objc fileprivate func showLocationDetails(_ sender: UIButton) {
+        performSegue(withIdentifier: "EditLocation", sender: sender)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EditLocation" {
+            let navCont = segue.destination as! UINavigationController
+            let locationDetailsVC = navCont.topViewController as! LocationDetailsViewController
+            locationDetailsVC.managedObjectContext = managedObjectContext
+
+            let button = sender as! UIButton
+            let location = locations[button.tag]
+            locationDetailsVC.locationToEdit = location
+        }
+    }
 }
 
 extension MapViewController: MKMapViewDelegate {
-    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is Location else {
+            return nil
+        }
+
+        let identifier = "Location"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+
+        if annotationView == nil {
+            let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+
+            pinView.isEnabled = true
+            pinView.canShowCallout = true
+            pinView.animatesDrop = false
+            pinView.pinTintColor = UIColor(red: 0.32, green: 0.82, blue: 0.4, alpha: 1)
+
+            let rightButton = UIButton(type: .detailDisclosure)
+            rightButton.addTarget(self, action: #selector(showLocationDetails), for: .touchUpInside)
+            pinView.rightCalloutAccessoryView = rightButton
+            annotationView = pinView
+        }
+
+        if let annotationView = annotationView {
+            annotationView.annotation = annotation
+
+            let button = annotationView.rightCalloutAccessoryView as! UIButton
+            if let index = locations.index(of: annotation as! Location) {
+                button.tag = index
+            }
+        }
+
+        return annotationView
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
