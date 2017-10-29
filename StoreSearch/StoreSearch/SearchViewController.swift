1194ab2a5da89fc9aebb9018e3fc8b976ab0944d
glebwell <gleb.maltcev@yandex.ru>
2017-10-29 13:22:36 +0300

    StoreSearch project was created

---
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Gleb on 29.10.17.
+//  Copyright © 2017 Gleb. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
