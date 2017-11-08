6b3d25925710486d469f5fa38f1c2e81cbd208a2
glebwell <gleb.maltcev@yandex.ru>
2017-11-08 16:44:42 +0300

    implemented until p.141

--- StoreSearch/StoreSearch/DetailViewController.swift
+++ StoreSearch/StoreSearch/DetailViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var kindLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var priceButton: UIButton!
+    
     @IBAction func close() {
         dismiss(animated: true, completion: nil)
     }
@@ -19,13 +27,24 @@ class DetailViewController: UIViewController {
         modalPresentationStyle = .custom
         transitioningDelegate = self
     }
-}
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.tintColor = UIColor(red: 20/255, green: 160/255, blue: 150/255, alpha: 1)
+        popupView.layer.cornerRadius = 10
+    }
+}
 
 extension DetailViewController: UIViewControllerTransitioningDelegate {
     func presentationController(forPresented presented: UIViewController,
                                 presenting: UIViewController?,
                                 source: UIViewController) -> UIPresentationController? {
         return DimmingPresentationController(presentedViewController: presented, presenting: presenting)
+    }
+}
+
+extension DetailViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return touch.view === self.view
     }
 }
